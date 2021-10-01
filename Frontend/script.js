818dc5dcbd01a2667159c9b4adb873671d4367dd
HamedJavedan <Javedan.Hamed@outlook.com>
2021-10-01 15:30:55 +0430

    1 OCT

--- Frontend/script.js
+++ Frontend/script.js
@@ -4,13 +4,17 @@ function nav(imgSrc) {
 	const navBrand = document.createElement('a')
 	const navImg = document.createElement('img')
 
-	nav.setAttribute('class', 'navbar navbar-light bg-light mb-4')
+	nav.setAttribute('class', 'navbar navbar-light bg-secondary mb-4')
 	navContain.setAttribute('class', 'container')
 	navBrand.setAttribute('class', 'navbar-brand')
+	navBrand.setAttribute('href', '#')
 	navImg.setAttribute('src', imgSrc)
+	navImg.setAttribute('height', '25')
+	navImg.setAttribute('alt', 'Orinoco')
 
 	nav.appendChild(navContain)
 	navContain.appendChild(navBrand)
+	navBrand.appendChild(navImg)
 
 	return nav
 }
@@ -23,7 +27,7 @@ const navPic = [
 	}
 ]
 
-function card(imgSrc) {
+function card(imgSrc, name, description, price,) {
 	const card = document.createElement('div')
 	const cardRow = document.createElement('div')
 	const cardImgCol = document.createElement('div')
@@ -37,13 +41,18 @@ function card(imgSrc) {
 	const cardPriceNo = document.createElement('h3')  
 	const cardBtn = document.createElement('a') 
 
-	cardBtn.innerHTML = "CLICK ME";
-	cardTitle.innerHTML = "hello";
+	cardBtn.innerHTML = 'view Product';
+	cardTitle.innerHTML = name;
+	cardText.innerHTML = description;
+	cardPriceNo.innerHTML = price;
+
 
 	card.setAttribute('class', 'card mb-3')
 	cardRow.setAttribute('class', 'row g-0')
 	cardImgCol.setAttribute('class', 'col-md-4')
 	cardImg.setAttribute('src', 'http://localhost:3000' + imgSrc)
+	cardImg.setAttribute('class', 'img-fluid rounded-start')
+	cardImg.setAttribute('alt', "")
 
 	cardBodCol.setAttribute('class', 'cal-mb-8')
 	cardBody.setAttribute('class', 'card-body')
@@ -51,14 +60,19 @@ function card(imgSrc) {
 	cardText.setAttribute('class', 'card-text')
 	cardPrice.setAttribute('class', 'text-muted')
 	cardPriceNo.setAttribute('class', '')
+	cardBtn.setAttribute('href', '#')
 	cardBtn.setAttribute('class', 'btn btn-primary stretched-link')
 
 	card.appendChild(cardRow)
-	cardRow.appendChild(cardImgCol , cardBodCol)
+	cardRow.appendChild(cardImgCol)
+	cardRow.appendChild(cardBodCol)
 	cardImgCol.appendChild(cardImg)
 
 	cardBodCol.appendChild(cardBody)
-	cardBody.appendChild(cardTitle , cardText , cardPrice , cardBtn)
+	cardBody.appendChild(cardTitle)
+	cardBody.appendChild(cardText)
+	cardBody.appendChild(cardPrice)
+	cardBody.appendChild(cardBtn)
 
 	cardPrice.appendChild(cardPriceNo)
 
@@ -67,46 +81,164 @@ function card(imgSrc) {
 
 const listOfTeddies = [
 	{
-		_id: 123,
-		name: 'Big ted',
-		price: 3500,
-		description: 'hello',
+		name: 'Small Teddy Bear',
+		price: '$10',
+		description: '【High Quality】This classic stuffed bear toy is made of high quality plush, which is really soft. And the inside filling is cotton, which is plump but not too tough. It is so good to be your kids or pets partner.',
 		imageUrl: '/images/teddy_1.jpg'
 	},
 	{
-		_id: 123,
-		name: 'Big ted',
-		price: 3500,
-		description: 'hello',
+		name: 'Cute Teddy Bear',
+		price: '$5',
+		description: '【Cute Plush Toys】With this plush toy has a cuddly appearance and 45*32 centimeters dimension',
 		imageUrl: '/images/teddy_2.jpg'
 	},
 	{
-		_id: 123,
-		name: 'Big ted',
-		price: 3500,
-		description: 'hello',
+		name: 'Twin Teddy Bear',
+		price: '$15',
+		description: 'Beautiful new design made from quality super soft cream plush material this really cute teddy bear has a big brown soft nose and cute red heart.',
 		imageUrl: '/images/teddy_3.jpg'
 	},
 	{
-		_id: 123,
-		name: 'Big ted',
-		price: 3500,
-		description: 'hello',
+		name: 'The T-shirt Teddy Bear',
+		price: '$20',
+		description: 'Lovely big Brown teddy bear with blue T-shirt.',
 		imageUrl: '/images/teddy_4.jpg'
 	},
 	{
-		_id: 123,
-		name: 'Big ted',
-		price: 3500,
-		description: 'hello',
+		name: 'Aurora Bonnie Teddy Bear',
+		price: '$3',
+		description: 'Soft and Cuddly, Great gift and Cute Smiley face with great price',
 		imageUrl: '/images/teddy_5.jpg'
 	}
 ]
 
+function footer(imgSrc) {
+	const footer = document.createElement('footer')
+	const footerOrg = document.createElement('div')
+	const footerOrgLogo = document.createElement('a')
+	const footerOrgLogoImg = document.createElement('img')
+	const footerOrgLogoName = document.createElement('span')
+
+	footer.setAttribute('class', 'd-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top')
+	footerOrg.setAttribute('class', 'col-md-4 d-flex align-items-center')
+	footerOrgLogo.setAttribute('class', 'navbar-brand')
+	footerOrgLogo.setAttribute('href', '#')
+	footerOrgLogoImg.setAttribute('src', imgSrc)
+	footerOrgLogoImg.setAttribute('height', '15')
+	footerOrgLogoImg.setAttribute('alt', 'Orinoco')
+	footerOrgLogoName.setAttribute('class', 'text-muted')
+
+	footerOrgLogoName.innerHTML = '© 2021 Company, Inc';
+
+	footer.appendChild(footerOrg)
+	footerOrg.appendChild(footerOrgLogo)
+	footerOrg.appendChild(footerOrgLogoName)
+	footerOrgLogo.appendChild(footerOrgLogoImg)
+
+	return footer
+}
+
+const footerP = [
+	{
+		name: 'logoImg',
+		imageUrl: 'https://user.oc-static.com/upload/2019/12/12/1576141243078_Orinoco%20logo.png'
+	}
+]
+
+function product(imgSrc, name, description, price,) {
+	const pro = document.createElement('div')
+	const proCont = document.createElement('div')
+	const proContHead = document.createElement('div')
+	const proContHeadTitle = document.createElement('h5')
+	const proContBody = document.createElement('div')
+	const proContBodyImg = document.createElement('Img')
+	const proContBody1 = document.createElement('div')
+	const proContBody1Text = document.createElement('P')
+	const proContBody1Price = document.createElement('Small')
+	const proContBody1PriceNo = document.createElement('h3')
+	const proContBtn = document.createElement('div')
+	const proContBtn1 = document.createElement('button')
+	const proContBtn2 = document.createElement('button')
+
+	pro.setAttribute('class', 'modal-dialog')
+	pro.setAttribute('role', 'document')
+	proCont.setAttribute('class', 'modal-content rounded-6 shadow')
+	proContHead.setAttribute('class', 'modal-header border-bottom-0')
+	proContHeadTitle.setAttribute('class', 'modal-title')
+	proContBody.setAttribute('class', 'modal-header border-bottom-0')
+	proContBodyImg.setAttribute('class', 'img-fluid rounded-start')
+	proContBodyImg.setAttribute('src', imgSrc)
+	proContBody1.setAttribute('class', 'modal-body py-0')
+	proContBody1Text.setAttribute('class', '')
+	proContBody1Price.setAttribute('class', 'text-muted')
+	proContBody1PriceNo.setAttribute('class', '')
+	proContBtn.setAttribute('class', 'modal-footer flex-column border-top-0')
+	proContBtn1.setAttribute('type', 'button')
+	proContBtn1.setAttribute('class', 'btn btn-lg btn-primary w-100 mx-0 mb-2')
+	proContBtn2.setAttribute('type', 'button')
+	proContBtn2.setAttribute('class', 'btn btn-lg btn-light w-100 mx-0')
+
+	pro.appendChild(proCont)
+	proCont.appendChild(proContHead)
+	proContHead.appendChild(proContHeadTitle)
+	proCont.appendChild(proContBody)
+	proContBody.appendChild(proContBodyImg)
+	proCont.appendChild(proContBody1)
+	proContBody1.appendChild(proContBody1Text)
+	proContBody1.appendChild(proContBody1Price)
+	proContBody1Price.appendChild(proContBody1PriceNo)
+	proCont.appendChild(proContBtn)
+	proContBtn.appendChild(proContBtn1)
+	proContBtn.appendChild(proContBtn2)
+
+	proContHeadTitle.innerHTML = name;
+	proContBody1PriceNo = price;
+	proContBody1Text = description;
+	proContBodyImg = price;
+	proContBtn1 = 'Add to Cart';
+	proContBtn2 = 'Buy Now';
+
+	return product
+}
+
+const TeddyPageList = [
+	{
+		name: 'Small Teddy Bear',
+		price: '$10',
+		description: '【High Quality】This classic stuffed bear toy is made of high quality plush, which is really soft. And the inside filling is cotton, which is plump but not too tough. It is so good to be your kids or pets partner.',
+		imageUrl: '/images/teddy_1.jpg'
+	},
+	{
+		name: 'Cute Teddy Bear',
+		price: '$5',
+		description: '【Cute Plush Toys】With this plush toy has a cuddly appearance and 45*32 centimeters dimension',
+		imageUrl: '/images/teddy_2.jpg'
+	},
+	{
+		name: 'Twin Teddy Bear',
+		price: '$15',
+		description: 'Beautiful new design made from quality super soft cream plush material this really cute teddy bear has a big brown soft nose and cute red heart.',
+		imageUrl: '/images/teddy_3.jpg'
+	},
+	{
+		name: 'The T-shirt Teddy Bear',
+		price: '$20',
+		description: 'Lovely big Brown teddy bear with blue T-shirt.',
+		imageUrl: '/images/teddy_4.jpg'
+	},
+	{
+		name: 'Aurora Bonnie Teddy Bear',
+		price: '$3',
+		description: 'Soft and Cuddly, Great gift and Cute Smiley face with great price',
+		imageUrl: '/images/teddy_5.jpg'
+	}
+]
+
+
 const productList = document.getElementById('product-list')
 for (let i = 0; i < listOfTeddies.length; i++) {
 	const teddy = listOfTeddies[i]
-	const teddyCard = card(teddy.imageUrl)
+	const teddyCard = card(teddy.imageUrl,teddy.name,teddy.description,teddy.price)
 	
 	productList.appendChild(teddyCard)
 }
@@ -114,10 +246,28 @@ for (let i = 0; i < listOfTeddies.length; i++) {
 const navElement = document.getElementById('nav-element')
 for (let i = 0; i < navPic.length; i++) {
 	const logo = navPic[i]
-	const navLogo = card(logo.imageUrl)
+	const navLogo = nav(logo.imageUrl)
 	
 	navElement.appendChild(navLogo)
 }
+
+const footerElement = document.getElementById('footer-element')
+for (let i = 0; i < footerP.length; i++) {
+	const logoImg = footerP[i]
+	const footerLogoImg = footer(logoImg.imageUrl)
+	
+	footerElement.appendChild(footerLogoImg)
+}
+
+const veiwProduct = document.getElementById('veiw-product')
+for (let i = 0; i < TeddyPageList.length; i++) {
+	const teddyPage = TeddyPageList[i]
+	const teddyPageCard = product(teddyPage.imageUrl,teddyPage.name,teddyPage.description,teddyPage.price)
+	
+	veiwProduct.appendChild(teddyPageCard)
+}
+
+
 
 
 
